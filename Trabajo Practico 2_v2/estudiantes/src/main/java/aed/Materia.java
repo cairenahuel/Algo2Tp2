a2c653b4652b7f0b534aabd302110a4480b5c845
cairenahuel <cairenahuel@gmail.com>
2024-06-15 13:14:15 -0300

    Agregada la clase PunteroMateriaYNombre para usar en la clase Materia, y agregada en la misma.

--- Trabajo Practico 2_v2/estudiantes/src/main/java/aed/Materia.java
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/Materia.java
@@ -8,13 +8,6 @@ public class Materia {
 
     private int contadorInscriptos;
     private ArrayList<String> listaInscriptos;
-    // TUPLA DE CARRERA
-    private ArrayList<Tuple> listaCarreraMateria = new ArrayList<Tuple>();
-
-    Class Tuple(Trie<Materia> carrera, String str) {
-        private Trie<Materia> carrera;
-
-    }
-
+    private ArrayList<PunteroMateriaYNombre> listaCarreraMateria;
     private Integer[] cargos = { 0, 0, 0, 0 };
 }
---
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/PunteroMateriaYNombre.java
@@ -0,0 +1,21 @@
+package aed;
+
+public class PunteroMateriaYNombre {
+
+    //Puntero hacia la carrera a la cual pertenece la materia
+    private Trie<Materia> punteroCarrera;
+
+    //Nombre especifico de esta materia en esta carrera
+    private String nombreEspecifico;
+
+    public PunteroMateriaYNombre(Trie<Materia> carrera, String nombre){
+        this.punteroCarrera=carrera;
+        this.nombreEspecifico=nombre;
+    }
+    public Trie<Materia> getInstanciaCarrera(){
+        return punteroCarrera;
+    }
+    public String getNombreEspecifico(){
+        return nombreEspecifico;
+    }
+}
