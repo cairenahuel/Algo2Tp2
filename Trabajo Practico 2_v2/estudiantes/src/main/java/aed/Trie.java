9ff3bc36e61a1a37bed584a1f9855ceb9bfd45cf
cairenahuel <cairenahuel@gmail.com>
2024-06-20 01:15:40 -0300

    Justificadas las complejidades del trie (menos de la funcion imprimir porque es recursiva)

--- Trabajo Practico 2_v2/estudiantes/src/main/java/aed/Trie.java
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/Trie.java
@@ -1,5 +1,4 @@
 package aed;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,10 +30,15 @@ public class Trie<T> {
         T valor = null;
 
         Nodo(T valorDado) {
+            // O(1)
             this.valor = valorDado;
-            int i = 0; // O(1)
-            while (i < 256) {// O(256)*(O(1)+O(1))
+            // O(1)
+            int i = 0;
+            // O(1) porque declare el largo del arreglo al construir el nodo.
+            while (i < 256) {
+                // O(1)
                 siguientes.add(null);
+                // O(1)
                 i++;
             }
         }
@@ -44,156 +48,286 @@ public class Trie<T> {
     int tamanio;
 
     public void definir(T valorDado, String donde) {
-        if (this.raiz == null) { // O(1)
-            raiz = new Nodo(null);// O(256)
+        // O(1)
+        if (this.raiz == null) {
+            // O(1)
+            raiz = new Nodo(null);
         }
-        int i = 0;// O(1)
-        Nodo actual = raiz;// O(1)
-        while (i < donde.length()) {// O(sum_{0}^{|donde|}) * O(1) acá no se si iria O(1) o O(256) o si como 256 es
-                                    // constante simplificamos a O(1)
+        // O(1)
+        int i = 0;
+        // O(1)
+        Nodo actual = raiz;
+        // O(1)
+        int largo = donde.length();
+        // O(sum_{0}^{largo - 1} de todo lo demas)
+        while (i < largo) {
+            // O(1)
             int numeroDelChar = (int) donde.charAt(i);
+            // O(1)
             Nodo siguiente = actual.siguientes.get(numeroDelChar);
 
+            // O(1)
             if (siguiente == null) {
 
-                if (i == donde.length() - 1) { // Si el siguiente es nulo y ademas estoy en el ultimo char, ese nodo
-                                               // va a ser el definido.
+                // O(1)
+                if (i == largo - 1) { // Si el siguiente es nulo y ademas estoy en el ultimo char, ese nodo
+                                      // va a ser el definido.
+                    // O(1)
                     Nodo nuevo = new Nodo(valorDado);
+                    // O(1)
                     actual.siguientes.set(numeroDelChar, nuevo);
+                    // O(1)
                     actual.hijos += 1;
+                    // O(1)
                     actual = nuevo;
+                    // O(1)
                     this.tamanio += 1;
                 } else { // Sino, creo un nuevo nodo y lo agrego al trie
+                    // O(1)
                     Nodo nuevo = new Nodo(null);
+                    // O(1)
                     actual.hijos += 1;
+                    // O(1)
                     actual.siguientes.set(numeroDelChar, nuevo);
+                    // O(1)
                     actual = nuevo;
                 }
 
             } else {
 
-                if (i == donde.length() - 1) { // Si hay siguiente nodo y ademas estoy en el ultimo char del
-                                               // string, defino este nodo.
+                // O(1)
+                if (i == largo - 1) { // Si hay siguiente nodo y ademas estoy en el ultimo char del
+                                      // string, defino este nodo.
+                    // O(1)
                     if (siguiente.valor == null) {
+                        // O(1)
                         this.tamanio += 1;
                     }
+                    // O(1)
                     siguiente.valor = valorDado;
                 } else {
+                    // O(1)
                     actual = siguiente;
                 }
             }
+            // O(1)
             i++;
         }
+
+        /*
+         * Entonces el metodo definir tiene una complejidad
+         * O(sum_{i=0}^{largo-1} 1 )
+         * que es equivalente a
+         * O(largo)
+         * y largo es la longitud del String donde
+         */
+
     }
 
     public boolean definido(String buscado) {
+        // O(1)
         Nodo actual = raiz;
+        // O(1)
         if (actual == null) {
+            // O(1)
             return false;
         }
 
+        // O(1)
         int i = 0;
-        while (i < buscado.length()) {
+        // O(1)
+        int largo = buscado.length();
+        // O(sum_{i=0}^{largo-1})
+        while (i < largo) {
+            // O(1)
             int numeroDelChar = (int) buscado.charAt(i);
+            // O(1)
             Nodo siguiente = actual.siguientes.get(numeroDelChar);
+            // O(1)
             if (i == buscado.length() - 1) {
+                // O(1)
                 return siguiente.valor != null;
             } else {
+                // O(1)
                 if (siguiente == null) {
+                    // O(1)
                     return false;
                 } else {
+                    // O(1)
                     actual = siguiente;
                 }
             }
+            // O(1)
             i++;
         }
+        // O(1)
         return false;
+
+        /*
+         * Entonces el metodo eliminar tiene una complejidad
+         * O(sum_{i=0}^{largo-1} 1 )
+         * que es equivalente a
+         * O(largo)
+         * y largo es la longitud del String buscado
+         */
+
     }
 
     public void eliminar(String buscado) {
+        // O(1)
         Nodo actual = raiz;
+        // O(1)
         Nodo ultimoSignificativo = raiz;
+        // O(1)
         Integer ultimoIndiceSignificativo = null;
+        // O(1)
         if (actual == null) {
+            // O(1)
             return;
         }
-
+        // O(1)
         int i = 0;
-        while (i < buscado.length()) {
+        // O(1)
+        int largo = buscado.length();
+        // O(sum_{i=0}^{largo-1} de todo lo demas)
+        while (i < largo) {
+            // O(1)
             int numeroDelChar = (int) buscado.charAt(i);
+            // O(1)
             Nodo siguiente = actual.siguientes.get(numeroDelChar);
-
+            // O(1)
             if (actual.hijos > 1 || actual.valor != null) {
+                // O(1)
                 ultimoSignificativo = actual;
+                // O(1)
                 ultimoIndiceSignificativo = i;
             }
-
-            if (i == buscado.length() - 1) {
+            // O(1)
+            if (i == largo - 1) {
+                // O(1)
                 if (siguiente == null) {
+                    // O(1)
                     return;
                 } else {
-                    if (siguiente.valor != null) {
-                        // si lo encuentra y no es nulo
+                    // O(1)
+                    if (siguiente.valor != null) { // si lo encuentra y no es nulo
+                        // O(1)
                         if (siguiente.hijos != 0) { // Si tiene hijos
+                            // O(1)
                             siguiente.valor = null;
+                            // O(1)
                             this.tamanio -= 1;
                         } else { // Si no tiene hijos
+                            // O(1)
                             if (ultimoSignificativo == raiz) {
+                                // O(1)
                                 if (raiz.hijos == 1) {
+                                    // O(1)
                                     this.raiz = null;
+                                    // O(1)
                                     this.tamanio -= 1;
+                                    // O(1)
                                     return;
                                 }
                             }
+                            // O(1)
                             int numCharSignificativo = buscado.charAt(ultimoIndiceSignificativo);
+                            // O(1)
                             ultimoSignificativo.siguientes.set(numCharSignificativo, null);
+                            // O(1)
                             ultimoSignificativo.hijos -= 1;
+                            // O(1)
                             this.tamanio -= 1;
                         }
                     }
                 }
             } else {
+                // O(1)
                 if (siguiente == null) {
+                    // O(1)
                     return;
                 } else {
+                    // O(1)
                     actual = siguiente;
                 }
             }
+            // O(1)
             i++;
         }
+
+        /*
+         * Entonces el metodo eliminar tiene una complejidad
+         * O(sum_{i=0}^{largo-1} 1 )
+         * que es equivalente a
+         * O(largo)
+         * y largo es la longitud del String buscado
+         */
+
     }
 
     public T obtener(String buscado) {
 
         // Si lo encuentra devuelve el valor del nodo, sino devuelve null
+
+        // O(1)
         if (raiz == null) {
+            // O(1)
             return null;
         }
+        // O(1) (es un atributo guardado)
         int largo = buscado.length();
+        // O(1)
         int i = 0;
+        // O(1)
         Nodo actual = raiz;
+        // O(sum_{i=0}^{largo-1} de todo lo siguiente)
         while (i < largo) {
+            // O(1)
             int indiceCaracter = (int) buscado.charAt(i);
+            // O(1)
             if (i == largo - 1) {
+                // O(1)
                 if (actual == null) {
+                    // O(1)
                     return null;
                 }
+                // O(1)
                 Nodo siguiente = actual.siguientes.get(indiceCaracter);
+                // O(1)
                 if (siguiente == null || siguiente.valor == null) {
+                    // O(1)
                     return null;
                 }
+                // O(1)
                 return siguiente.valor;
             } else {
+                // O(1)
                 if (actual == null) {
+                    // O(1)
                     return null;
                 }
+                // O(1)
                 Nodo siguiente = actual.siguientes.get(indiceCaracter);
+                // O(1)
                 actual = siguiente;
             }
+            // O(1)
             i++;
         }
+        // O(1)
         return null;
+
+        /*
+         * Entonces el metodo obtener tiene una complejidad
+         * O(sum_{i=0}^{largo-1} 1 )
+         * que es equivalente a
+         * O(largo)
+         * y largo es la longitud del String buscado
+         */
     }
+
+
+    // No te se justificar la complejidad del imprimir pues es recursiva
 
     public String[] imprimir() {
         List<String> cadena = new ArrayList<>();
@@ -208,7 +342,7 @@ public class Trie<T> {
 
         int i = 0;
         int hijosEncontrados = 0;
-        while (i < 255 && hijosEncontrados < nodo.hijos) {
+        while (i < 256 && hijosEncontrados < nodo.hijos) {
 
             Nodo actual = nodo.siguientes.get(i);
 
@@ -227,18 +361,6 @@ public class Trie<T> {
 
             i++;
         }
-
-        // en general creo que las complejidades dan
-
-        // por si quieren testearlo
-
-        // public static void main(String[] args) {
-        // Trie<Integer> trie = new Trie<Integer>();
-        // trie.definir(1, "que");
-        // trie.definir(2, "que");
-        // trie.definir(4, "peso");
-        // trie.inorder();
-        // }
 
     }
 }
